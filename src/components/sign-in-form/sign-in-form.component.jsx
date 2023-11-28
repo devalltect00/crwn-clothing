ace6a4e931d71834032cfd14071121dbc58389c5
devalltect00 <rizkyslenovo29@gmail.com>
2023-11-28 11:39:18 +0700

    created authetication page component alongside sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,8 @@ import Button from '../button/Button.component'
 
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword
 } from "../../utils/firebase/firebase.utils";
 
 import './sign-in-form.styles.scss'
@@ -24,44 +25,81 @@ const SignInForm = () => {
   }
 
 
-  const submitHandler = () => {
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try{
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response)
+      resetFormFields();
+    }catch(error){
+      switch(error.code){
+        case 'auth/wrong-pasword':
+          alert('incorrect password for email')
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        case 'auth/invalid-login-credentials':
+          alert('invalid login credentials')
+          break;
+        default:
+          console.log(error)
+      }
+    }
   }
 
-  const changeHandler = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name] : value});
   }
 
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
+  const signInWithGoogle = async () => {
+    try {
+      const {user} = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user)
+    } catch (error) {
+      if (error.code === 'auth/popup-closed-by-user'){
+        console.log('Google sign-in popup closed by user');
+      } else {
+        console.error('Error signing in with Google:', error.message);
+      }
+    }
+    
   }
 
   return (
     <div className="sign-in-container">
-      <h2>I already have an account</h2>
+      <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={handleSubmit}>
         <FormInput
           label='Email'
           type='email'
           required
-          onChange={changeHandler}
+          onChange={handleChange}
           name='email'
           value={email}
         />
+        
         <FormInput
           label='Password'
           type='password'
           required
-          onChange={changeHandler}
+          onChange={handleChange}
           name='password'
           value={password}
         />
-        <Button>sign in</Button>
-        <Button onClick={logGoogleUser} buttonType={'google'}>sign in with google</Button>
+        <div className='buttons-container'>
+          <Button type='submit'>sign in</Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
       </form>
     </div>
   )
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,15 +1,15 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import './sign-in.styles.scss'
+import './authentication.styles.scss'
 
-const SignIn = () => {
+const Authentication = () => {
   return (
-    <div>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
   )
 }
 
-export default SignIn;
+export default Authentication;
